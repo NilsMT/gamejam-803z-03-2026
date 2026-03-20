c93f762166b987c72824f47b6e4b4cf54bfb8b4b
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-03-20 23:56:31 +0100

    re-init (everything suck)

--- tangled/character/enemy/enemy.gd
+++
@@ -1,7 +0,0 @@
-extends "res://character/base_character.gd"  # Inherit from base_character.gd
-
-func _get_input_direction() -> Vector2:  # Override for AI movement
-	var player = get_tree().get_first_node_in_group("players")
-	if player:
-		return (player.global_position - global_position).normalized()
-	return Vector2.ZERO
