0a4cda26e667d09de7d6f635c3fc66b07c838d42
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-03-21 03:43:12 +0100

    fix animation broken burh (just change them after dude)

--- tangled/mob.gd
+++ tangled/mob.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var health = 3
+const DAMAGE = 5
+const SPEED = 300.0
 
 @onready var player = get_node("/root/Game/Player")
 
@@ -9,7 +11,8 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 300.0
+	velocity = direction * SPEED
+	velocity *= delta
 	move_and_slide()
 	
 
